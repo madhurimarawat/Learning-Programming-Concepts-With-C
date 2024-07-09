ddfc38a1fcc7623a0344da532232d0219c656f29
Madhurima Rawat <105432776+madhurimarawat@users.noreply.github.com>
2024-07-09 12:49:23 +0530

    Update Alphabet-Pattern-EDCBA-Inverse-space-3.c
    Formatted the code for better readability

--- Alphabet-Pattern/Alphabet-Pattern-EDCBA-Inverse-space-3.c
+++ Alphabet-Pattern/Alphabet-Pattern-EDCBA-Inverse-space-3.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 
 // Program for printing pattern
 
 /*
 
-For r=E
+For r = E
 
     E
    DD
@@ -14,19 +14,23 @@ AAAAA
 
 */
 
-void main(){
-char i,j,s,r;
-char k= 'A';
-printf("Enter Ending character of rows: ");
-scanf("%c",&r);
-printf("\n");
-for(i=r;i>='A';i--){
-for(s=1;s<=i;s++){
-printf(" ");
-}
-for(j=i;j<=r;j++){
-printf("%c",i);
-}
-printf("\n");
-}
+void main()
+{
+    char i, j, s, r;
+    char k = 'A';
+    printf("Enter Ending character of rows: ");
+    scanf("%c", &r);
+    printf("\n");
+    for (i = r; i >= 'A'; i--)
+    {
+        for (s = 1; s <= i; s++)
+        {
+            printf(" ");
+        }
+        for (j = i; j <= r; j++)
+        {
+            printf("%c", i);
+        }
+        printf("\n");
+    }
 }
