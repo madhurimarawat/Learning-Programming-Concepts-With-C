c68c9c0200b264e3406c10c8dcae0a391e8c2041
Madhurima Rawat <105432776+madhurimarawat@users.noreply.github.com>
2024-07-09 12:54:27 +0530

    Update Recursion-factorial.c
    Formatted the code and added comments for better readability

--- Recursion/Recursion-factorial.c
+++ Recursion/Recursion-factorial.c
@@ -1,13 +1,22 @@
-#include<stdio.h>
-int fact(int n){
-if(n==0)
-return 1;
-else
-return n*fact(n-1);
-  }
-void main(){
+// This program finds the Factorial of a number using recursion
+
+// Importing Header File
+#include <stdio.h>
+
+// Function to find factorial
+int fact(int n)
+{
+  if (n == 0)
+    return 1;
+  else
+    return n * fact(n - 1);
+}
+
+// Main function
+void main()
+{
   int n;
-printf("enter number");
-scanf("%d",&n);
-printf("Factorial is %d",fact(n));
+  printf("enter number");
+  scanf("%d", &n);
+  printf("Factorial is %d", fact(n));
 }
